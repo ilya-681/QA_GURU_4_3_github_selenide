c1b31c75656db37087e008b87695eecb06cd011d
ilya <ilya-681@ya.ru>
2021-02-13 16:56:36 +0300

    After code cleaning

--- src/test/java/DragNDropTest.java
+++ src/test/java/DragNDropTest.java
@@ -1,18 +1,16 @@
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selectors.byText;
+
 import static com.codeborne.selenide.Selenide.*;
 
-public class Drag_n_Drop_Test {
+public class DragNDropTest {
 
     @Test
     void dragAndDrop() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
         actions().clickAndHold($("#column-a")).moveByOffset(150, 10).release().perform();
+        //assert
         $("#column-b").shouldHave(Condition.text("A"));
-
     }
-
-
 }
--- src/test/java/GihubSelenideTest.java
+++ src/test/java/GihubSelenideTest.java
@@ -1,24 +1,22 @@
-import com.codeborne.selenide.Selenide;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
-import java.io.File;
+import org.junit.jupiter.api.Test;
+
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
-public class Gihub_Selenide_Test {
+public class GihubSelenideTest {
     @Test
     void enableJUnit5InSoftAssertions() {
         open("https://github.com/");
         $(byName("q")).setValue("Selenide").pressEnter();
-        $(".repo-list").$(byLinkText("selenide/selenide")).click();
+        $("ul.repo-list a").click();
+        //вариант до правки $(".repo-list").$(byLinkText("selenide/selenide")).click();
         // вариант из лекции показался усложенным $$("ul.repo-list li").first().$("a").click();
         $("[data-content=Wiki]").click();
         $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
         $("#wiki-pages-box").$(byText("SoftAssertions")).click();
         $("body").shouldHave(text("JUnit5 extension - "));
-
     }
 }
